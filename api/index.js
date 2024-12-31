f57d24004c1502958180b65ba140c033c416789e
Minkesh Jain <minkesh@pidilite.vc>
2024-12-31 18:19:02 +0530

    added cors config in handler

--- api/index.js
+++ api/index.js
@@ -1,7 +1,32 @@
 import express from "express";
 import fetchUserData from "./fetch-user-data.js";
 import uploadEvent from "./upload-user-event.js";
+
+// List of allowed origins
+const allowedOrigins = [
+  "https://letsmultiply.co.in",
+  "https://stage.letsmultiply.co.in",
+  "https://dev.letsmultiply.co.in",
+  "http://localhost:3000",
+];
+
 const app = express();
+
+// CORS middleware configuration
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "OPTIONS"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type"], // Allowed headers
+  })
+);
 
 app.use(express.json()); // Add this line
 
