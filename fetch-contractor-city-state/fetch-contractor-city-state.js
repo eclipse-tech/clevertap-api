2a2032246ff5fca8b411c54d51d7c7d5f0cf59a7
Minkesh Jain <minkesh@pidilite.vc>
2025-02-19 17:45:59 +0530

    added city and state fetch script

---
+++ fetch-contractor-city-state/fetch-contractor-city-state.js
@@ -0,0 +1,40 @@
+import fs from "fs";
+import csvParser from "csv-parser";
+
+import fetchUserData from "../api/fetch-user-data.js";
+
+const inputFile = "./contractor-city-state-input.csv";
+const outputFile = "./contractor-city-state-output.csv";
+
+// Write the header to the output file
+const header = "Mobile Number, City, State";
+
+fs.writeFileSync(outputFile, header);
+
+const mobileNumbers = [];
+fs.createReadStream(inputFile)
+  .pipe(csvParser())
+  .on("data", (row) => {
+    mobileNumbers.push(row["Mobile Number"]);
+  })
+  .on("end", async () => {
+    for (const mobileNumber of mobileNumbers) {
+      try {
+        const { profileData } = await fetchUserData(mobileNumber);
+
+        // // Extract the required information from the API response
+        // const requiredInfo = response.data.requiredInfo;
+
+        const { city, state } = profileData;
+        // // Append the data row to the output CSV file
+        fs.appendFileSync(outputFile, `\n${mobileNumber},${city},${state}`);
+
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+      } catch (error) {
+        fs.appendFileSync(outputFile, `\n${mobileNumber},NA,NA`);
+        console.error("Profile not found ");
+      }
+    }
+
+    console.log("CSV parsing and API calls complete");
+  });
